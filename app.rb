622f75464bb4b125d2972a59a645b74c6c6c6679
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-07 17:39:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do  #renders a form that can POST a name, breed, and age
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+
+     erb :display_puppy
+
+     # recieves a request from /new at /puppy
+     #displays the puppy information dynamically
+  end
 end
